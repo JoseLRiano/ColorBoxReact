f2ff2739a46da58b14bb884b3b7a733d9268fb04
JoseLRiano <joselriano@yahoo.com>
2020-05-18 20:18:21 -0400

    Updates

---
+++ precache-manifest.fda86efc4e3dc54ca73e7d72be301c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38c2c82b6dad9c5015d9af6414e5e03",
+    "url": "/ColorBoxReact/index.html"
+  },
+  {
+    "revision": "8ca799819e9f785a29f6",
+    "url": "/ColorBoxReact/static/css/main.be134679.chunk.css"
+  },
+  {
+    "revision": "0bc9c8b25e4ac9a49855",
+    "url": "/ColorBoxReact/static/js/2.27e25d7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ColorBoxReact/static/js/2.27e25d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca799819e9f785a29f6",
+    "url": "/ColorBoxReact/static/js/main.0bd361b6.chunk.js"
+  },
+  {
+    "revision": "72a9a6f6fce8609a4074",
+    "url": "/ColorBoxReact/static/js/runtime-main.4949140e.js"
+  }
+]);
